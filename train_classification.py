8c3a884dff04aa5c8c660dda8e66ee44640304c5
Jianxiong Cai <ernestme@icloud.com>
2018-05-04 10:46:11 +0800

    Add tey-except blovk to deal with OSError

--- train_classification.py
+++ train_classification.py
@@ -68,30 +68,33 @@ classifier.cuda()
 num_batch = len(dataset)/opt.batchSize
 
 for epoch in range(opt.nepoch):
-    for i, data in enumerate(dataloader, 0):
-        points, target = data
-        points, target = Variable(points), Variable(target[:,0])
-        points = points.transpose(2,1)
-        points, target = points.cuda(), target.cuda()
-        optimizer.zero_grad()
-        pred, _ = classifier(points)
-        loss = F.nll_loss(pred, target)
-        loss.backward()
-        optimizer.step()
-        pred_choice = pred.data.max(1)[1]
-        correct = pred_choice.eq(target.data).cpu().sum()
-        print('[%d: %d/%d] train loss: %f accuracy: %f' %(epoch, i, num_batch, loss.data[0], correct/float(opt.batchSize)))
-
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
+    try:
+        for i, data in enumerate(dataloader, 0):
             points, target = data
             points, target = Variable(points), Variable(target[:,0])
             points = points.transpose(2,1)
             points, target = points.cuda(), target.cuda()
+            optimizer.zero_grad()
             pred, _ = classifier(points)
             loss = F.nll_loss(pred, target)
+            loss.backward()
+            optimizer.step()
             pred_choice = pred.data.max(1)[1]
             correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' %(epoch, i, num_batch, blue('test'), loss.data[0], correct/float(opt.batchSize)))
+            print('[%d: %d/%d] train loss: %f accuracy: %f' %(epoch, i, num_batch, loss.data[0], correct/float(opt.batchSize)))
+
+            if i % 10 == 0:
+                j, data = next(enumerate(testdataloader, 0))
+                points, target = data
+                points, target = Variable(points), Variable(target[:,0])
+                points = points.transpose(2,1)
+                points, target = points.cuda(), target.cuda()
+                pred, _ = classifier(points)
+                loss = F.nll_loss(pred, target)
+                pred_choice = pred.data.max(1)[1]
+                correct = pred_choice.eq(target.data).cpu().sum()
+                print('[%d: %d/%d] %s loss: %f accuracy: %f' %(epoch, i, num_batch, blue('test'), loss.data[0], correct/float(opt.batchSize)))
+    except OSError, e:
+        print "[WARNING] catch OSError: " + e
 
     torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
